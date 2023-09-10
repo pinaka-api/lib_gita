53aa03df3ee98a07d5891035dcdebdd92af3e31e
externref <shiva02939@gmail.com>
2023-09-10 12:45:11 +0530

    add README

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # lib_gita
 
 Library for Gita handler.
+
+## How to Contribute
+
+Contributions are welcomed ...
+
+To add a shloka in a particular adhyaya
+
+- open the adhyaya file in the src folder
+- in the default export object, add the shloka in this format
+
+```ts
+{
+    shloka_number: new Shloka(
+        adhyaya_number,
+        shloka_number,
+        speakers.<speaker>, // speaker is an object containing speakers in the gita.
+        original_shloka,
+        romanised_form,
+        hindi_translation,
+        english_translation
+    )
+}
+```
+
+Translations from gitasupersite.iitk.ac.in are used:
+
+- hindi: Swami Ramsukhdas
+- english: Swami Sivananda
+
+Ref url: https://www.gitasupersite.iitk.ac.in/srimad?language=dv&field_chapter_value=1&field_nsutra_value=1&htrskd=1&etsiva=1&choose=1
--- src/Shloka.ts
+++ src/Shloka.ts
@@ -1,29 +1,15 @@
 import { ShlokaDict } from "..";
 
 export class Shloka {
-  adhyaya: Number;
-  shloka: Number;
-  speaker: String;
-  original: String;
-  romanised: String;
-  hindi: String;
-  english: String;
   constructor(
-    adhyaya: Number,
-    shloka: Number,
-    speaker: String,
-    original: String,
-    romanised: String,
-    hindi: String,
-    english: String,
+    public adhyaya: Number,
+    public shloka: Number,
+    public speaker: String,
+    public original: String,
+    public romanised: String,
+    public hindi: String,
+    public english: String,
   ) {
-    this.adhyaya = adhyaya;
-    this.shloka = shloka;
-    this.speaker = speaker;
-    this.original = original;
-    this.romanised = romanised;
-    this.hindi = hindi;
-    this.english = english;
   }
   dict(): ShlokaDict {
     return {
